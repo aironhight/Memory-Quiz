724e7a855d453fa57687c64257259a2ae46e5224
aironhight <aironhight@yahoo.com>
2018-06-02 13:01:58 +0200

    Malko rabota

---
+++ Memory Quiz/Assets/_Scripts/DBManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DBManager : MonoBehaviour {
+
+	private DatabaseReference dbr;
+
+	private void Awake() {
+		dbr = FirebaseDatabase.DefaultInstance.GetReference("highscore");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Memory Quiz/Assets/_Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	public int currentScore;
+
+
+	// Use this for initialization
+	void Start () {
+		currentScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void postScore() {
+
+	}
+
+	public Score[] getTopScores() {
+		return null;
+	}
+
+	public void addScore (int score) {
+		currentScore += score;
+	}
+}
